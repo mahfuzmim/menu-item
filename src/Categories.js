2010be1f303d29ec473d5414320cb787852ce21b
mahfuzmim <mahuzurrahman@gmail.com>
2023-01-31 21:25:34 +0600

    first commit

---
+++ src/Categories.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Categories = ({ categories, filterItems }) => {
+  console.log(categories);
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        console.log(category);
+        return (
+          <button
+            type="button"
+            className="filter-btn"
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
